5269b80b1cf4f0d43f9008900cf3d70e0959f7e0
kmjyz44 <kmjyz44@gmail.com>
2022-07-23 07:09:52 +0300

    test

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -54,8 +54,9 @@ function getFizzBuzz(num) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  return (n != 1) ? n * getFactorial(n - 1) : 1;
+
 }
 
 
